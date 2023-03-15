b1ec06c39a7221381551dd17da626b2f1c043726
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-15 16:59:40 -0300

    setup out stdin by creating two function

--- play.js
+++ play.js
@@ -1,6 +1,29 @@
 //sayHello and myKey are just to show extra exampes for comprehension from the exports file
 const {connect /*sayHello, myKey*/ } = require('./client');
 
-
 console.log("Connecting ...");
 connect();
+
+// stdin.on('data', (key) => {
+//   process.stdout.write('.');
+// });
+
+const handleUserInput = function (key) {
+
+  // your code here
+  if ( key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
+
