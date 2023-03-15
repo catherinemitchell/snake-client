84a65d9347cd5a91dc1138d3e4dfd1586ed53fdc
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-15 17:13:18 -0300

    made a few tweaks to clean up the code

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
@@ -11,20 +11,23 @@ const connect = function () {
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
-  
+
   conn.on("connect", () => {
-    conn.write("Name: CLM")
+    conn.write("Name: CLM");
     console.log("Successfully connected to game server");
 
+    // conn.write("Move: up");
     
+
+
   });
+
+
   return conn;
 
 };
-// const sayHello = () => console.log("hello"); ----> this is just to show extra exampes for the object
-// const myKey = [1,2,3]  ---> this is just to show extra examples for the object
+
 module.exports = {
   connect,
-  // sayHello,
-  // myKey,
-}
+  
+};
