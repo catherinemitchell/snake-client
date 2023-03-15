19390cffadc314d70a59097c14fef7171c844932
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-15 15:38:12 -0300

    imported client.js file and added shorthand

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+  
+
+  conn.on("connect", () => {
+    console.log("Connected");
+  });
+  return conn;
+
+};
+// const sayHello = () => console.log("hello"); ----> this is just to show extra exampes for the object
+// const myKey = [1,2,3]  ---> this is just to show extra examples for the object
+module.exports = {
+  connect,
+  // sayHello,
+  // myKey,
+}
--- play.js
+++ play.js
@@ -1,24 +1,5 @@
-const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
-
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-  
-
-  conn.on("connect", () => {
-    console.log("Connected");
-  });
-  return conn;
-
-};
+//sayHello and myKey are just to show extra exampes for comprehension from the exports file
+const {connect /*sayHello, myKey*/ } = require('./client');
 
 
 console.log("Connecting ...");
